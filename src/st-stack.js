68cb41fa92873536d1f3d02049d33ea604420846
Katia <katia.zakharina@gmail.com>
2021-10-14 23:34:46 +0300

    feat: add stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -1,30 +1,18 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   //FILO
+  constructor(){
+    this.value=[];
+  }
+
+  push(element) {
+    return this.value.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.value.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.value.slice().pop();
   }
 }
