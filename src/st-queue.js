1b5da71d0ee4dcad0c86a0c9656bf2dcd6e7c0fe
Katia <katia.zakharina@gmail.com>
2021-10-17 11:35:16 +0300

    feat: add queue

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,15 +1,35 @@
 const { ListNode } = require('../extensions/list-node.js');
 
 module.exports = class Queue {
+  constructor(){
+    this.value=null;
+  }
 
   getUnderlyingList() {
+    return this.value;
   }
 
   enqueue(value) {
+    let current=this.value;
+    while(current && current.next){
+      current=current.next;
+    }
+
+    if(current===null){
+      this.value=new ListNode(value);
+    } else {
+      current.next=new ListNode(value);
+    }
   }
 
   dequeue() {
+    let first=this.value,
+    second=first.next;
+    const deleted=this.value.value;
 
+    first.value=second.value;
+    first.next=second.next;
+    
+    return deleted;
   }
-
 }
